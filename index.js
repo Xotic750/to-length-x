c3782892f54ca68ffca55eb8c56b2c35e65e8069
Graham Fairweather <graham.fairweather@ispy.se>
2017-03-27 11:31:19 +0200

    Remove yarn and jscs, cleanup

--- index.js
+++ index.js
@@ -22,51 +22,29 @@
  * alt="npm version" height="18">
  * </a>
  *
- * toLength module.
+ * ToLength converts argument to an integer suitable for use as the length of an array-like object.
  *
- * <h2>ECMAScript compatibility shims for legacy JavaScript engines</h2>
- * `es5-shim.js` monkey-patches a JavaScript context to contain all EcmaScript 5
- * methods that can be faithfully emulated with a legacy JavaScript engine.
+ * Requires ES3 or above.
  *
- * `es5-sham.js` monkey-patches other ES5 methods as closely as possible.
- * For these methods, as closely as possible to ES5 is not very close.
- * Many of these shams are intended only to allow code to be written to ES5
- * without causing run-time errors in older engines. In many cases,
- * this means that these shams cause many ES5 methods to silently fail.
- * Decide carefully whether this is what you want. Note: es5-sham.js requires
- * es5-shim.js to be able to work properly.
+ * @see {@link http://www.ecma-international.org/ecma-262/6.0/#sec-tolength|7.1.15 ToLength ( argument )}
  *
- * `json3.js` monkey-patches the EcmaScript 5 JSON implimentation faithfully.
- *
- * `es6.shim.js` provides compatibility shims so that legacy JavaScript engines
- * behave as closely as possible to ECMAScript 6 (Harmony).
- *
- * @version 1.1.0
+ * @version 1.1.1
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
  * @module to-length-x
  */
 
-/* jslint maxlen:80, es6:true, white:true */
-
-/* jshint bitwise:true, camelcase:true, curly:true, eqeqeq:true, forin:true,
-   freeze:true, futurehostile:true, latedef:true, newcap:true, nocomma:true,
-   nonbsp:true, singleGroups:true, strict:true, undef:true, unused:true,
-   es3:false, esnext:true, plusplus:true, maxparams:1, maxdepth:1,
-   maxstatements:3, maxcomplexity:2 */
-
 /* eslint strict: 1, max-statements: 1 */
 
-/* global require, module */
+/* global module */
 
 ;(function () { // eslint-disable-line no-extra-semi
 
   'use strict';
 
   var toInteger = require('to-integer-x');
-  var MAX_SAFE_INTEGER = Number.MAX_SAFE_INTEGER;
-  var zero = 0;
+  var MAX_SAFE_INTEGER = 9007199254740991;
 
   /**
    * Converts `value` to an integer suitable for use as the length of an
@@ -83,8 +61,8 @@
    */
   module.exports = function toLength(value) {
     var len = toInteger(value);
-    if (len <= zero) {
-      return zero;
+    if (len <= 0) {
+      return 0;
     } // includes converting -0 to +0
     if (len > MAX_SAFE_INTEGER) {
       return MAX_SAFE_INTEGER;

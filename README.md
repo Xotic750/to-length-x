3c2636bae797bc07439de83a7c6da815c8e2325c
Xotic750 <xotic750@gmail.com>
2017-10-26 07:05:59 +0200

    Update for ES5 to ES2018 white space specifications

--- README.md
+++ README.md
@@ -21,20 +21,32 @@
 <a name="module_to-length-x"></a>
 
 ## to-length-x
-ES6-compliant shim for ToLength.
+Shim for ToLength.
 
 **See**: [7.1.15 ToLength ( argument )](http://www.ecma-international.org/ecma-262/6.0/#sec-tolength)  
-**Version**: 2.1.0  
+**Version**: 3.0.0  
 **Author**: Xotic750 <Xotic750@gmail.com>  
 **License**: [MIT](&lt;https://opensource.org/licenses/MIT&gt;)  
 **Copyright**: Xotic750  
-<a name="exp_module_to-length-x--module.exports"></a>
 
-### `module.exports(value)` ⇒ <code>number</code> ⏏
+* [to-length-x](#module_to-length-x)
+    * [`.toLength`](#module_to-length-x.toLength)
+    * [`.toLength2016`](#module_to-length-x.toLength2016) ⇒ <code>number</code>
+    * [`.toLength2018`](#module_to-length-x.toLength2018) ⇒ <code>number</code>
+
+<a name="module_to-length-x.toLength"></a>
+
+### `to-length-x.toLength`
+Reference to toLength2018.
+
+**Kind**: static property of [<code>to-length-x</code>](#module_to-length-x)  
+<a name="module_to-length-x.toLength2016"></a>
+
+### `to-length-x.toLength2016` ⇒ <code>number</code>
 Converts `value` to an integer suitable for use as the length of an
-array-like object.
+array-like object. (ES2016)
 
-**Kind**: Exported function  
+**Kind**: static property of [<code>to-length-x</code>](#module_to-length-x)  
 **Returns**: <code>number</code> - Returns the converted integer.  
 
 | Param | Type | Description |
@@ -43,7 +55,28 @@ array-like object.
 
 **Example**  
 ```js
-var toLength = require('to-length-x');
+var toLength = require('to-length-x').toLength2016;
+toLength(3); // 3
+toLength(Number.MIN_VALUE); // 0
+toLength(Infinity); // Number.MAX_SAFE_INTEGER
+toLength('3'); // 3
+```
+<a name="module_to-length-x.toLength2018"></a>
+
+### `to-length-x.toLength2018` ⇒ <code>number</code>
+Converts `value` to an integer suitable for use as the length of an
+array-like object. (ES2018)
+
+**Kind**: static property of [<code>to-length-x</code>](#module_to-length-x)  
+**Returns**: <code>number</code> - Returns the converted integer.  
+
+| Param | Type | Description |
+| --- | --- | --- |
+| value | <code>\*</code> | The value to convert. |
+
+**Example**  
+```js
+var toLength = require('to-length-x').toLength2018;
 toLength(3); // 3
 toLength(Number.MIN_VALUE); // 0
 toLength(Infinity); // Number.MAX_SAFE_INTEGER
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
- * @file ES6-compliant shim for ToLength.
+ * @file Shim for ToLength.
  * @see {@link http://www.ecma-international.org/ecma-262/6.0/#sec-tolength|7.1.15 ToLength ( argument )}
- * @version 2.1.0
+ * @version 3.0.0
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
@@ -10,24 +10,13 @@
 
 'use strict';
 
-var toInteger = require('to-integer-x');
+var libToInteger = require('to-integer-x');
+var toInteger2016 = libToInteger.toInteger2016;
+var toInteger2018 = libToInteger.toInteger2018;
 var MAX_SAFE_INTEGER = require('max-safe-integer');
 
-/**
- * Converts `value` to an integer suitable for use as the length of an
- * array-like object.
- *
- * @param {*} value - The value to convert.
- * @returns {number} Returns the converted integer.
- * @example
- * var toLength = require('to-length-x');
- * toLength(3); // 3
- * toLength(Number.MIN_VALUE); // 0
- * toLength(Infinity); // Number.MAX_SAFE_INTEGER
- * toLength('3'); // 3
- */
-module.exports = function toLength(value) {
-  var len = toInteger(value);
+var $toLength2016 = function toLength2016(value) {
+  var len = toInteger2016(value);
   // includes converting -0 to +0
   if (len <= 0) {
     return 0;
@@ -38,4 +27,55 @@ module.exports = function toLength(value) {
   }
 
   return len;
+};
+
+var $toLength2018 = function toLength2018(value) {
+  var len = toInteger2018(value);
+  // includes converting -0 to +0
+  if (len <= 0) {
+    return 0;
+  }
+
+  if (len > MAX_SAFE_INTEGER) {
+    return MAX_SAFE_INTEGER;
+  }
+
+  return len;
+};
+
+module.exports = {
+  /**
+   * Reference to toLength2018.
+   */
+  toLength: $toLength2018,
+
+  /**
+   * Converts `value` to an integer suitable for use as the length of an
+   * array-like object. (ES2016)
+   *
+   * @param {*} value - The value to convert.
+   * @returns {number} Returns the converted integer.
+   * @example
+   * var toLength = require('to-length-x').toLength2016;
+   * toLength(3); // 3
+   * toLength(Number.MIN_VALUE); // 0
+   * toLength(Infinity); // Number.MAX_SAFE_INTEGER
+   * toLength('3'); // 3
+   */
+  toLength2016: $toLength2016,
+
+  /**
+   * Converts `value` to an integer suitable for use as the length of an
+   * array-like object. (ES2018)
+   *
+   * @param {*} value - The value to convert.
+   * @returns {number} Returns the converted integer.
+   * @example
+   * var toLength = require('to-length-x').toLength2018;
+   * toLength(3); // 3
+   * toLength(Number.MIN_VALUE); // 0
+   * toLength(Infinity); // Number.MAX_SAFE_INTEGER
+   * toLength('3'); // 3
+   */
+  toLength2018: $toLength2018
 };

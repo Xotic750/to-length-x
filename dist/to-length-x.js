edaff78e798296857d4d9e644a385e485a9de329
Graham Fairweather <xotic750@gmail.com>
2019-08-04 21:34:15 +0200

    Auto run

--- dist/to-length-x.js
+++ dist/to-length-x.js
@@ -2,7 +2,7 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-2017",
-  "date": "2019-08-04T14:57:48.512Z",
+  "date": "2019-08-04T19:34:09.026Z",
   "describe": "",
   "description": "Shim for ToLength.",
   "file": "to-length-x.js",
